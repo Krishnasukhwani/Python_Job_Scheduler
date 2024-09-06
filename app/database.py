8761c78a71247a0d42c57da7307bcf652c914678
Krishna Sukhwani <97449349+Krishnasukhwani@users.noreply.github.com>
2024-09-06 16:41:59 +0530

    Updated database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 # MongoDB connection settings
-MONGO_DETAILS = "mongodb://localhost:27017"  # Update this with your MongoDB URI
+MONGO_DETAILS = "mongodb+srv://krishnasukhwani64:<db_password>@cluster0.om8gr.mongodb.net/"  # Update this with your MongoDB URI
 client = MongoClient(MONGO_DETAILS)
 database = client.jobs_db  
 jobs_collection = database.jobs  
